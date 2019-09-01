22344039ebf06d2258002f9e6f29eacaaf719e41
Théo Malaper <theo@Theos-MacBook-Pro-2.local>
2019-09-01 16:04:06 -0400

    nearly finished

--- fetcher.js
+++ fetcher.js
@@ -1,13 +1,32 @@
-const url = process.argv.slice(2);
+const fs = require('fs');
 const request = require("request");
+const readline = require('readline');
 
-const fetcher = (url) => {
-  request(url[0], (error, response, body) => {
-    console.log("error:", error)
-    console.log("status code:", response && response.statusCode)
-    console.log("body:", body)
-    console.log(response.socket[Symbol(bytesRead)])
+const url = process.argv[2]
+const path = process.argv[3];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const fetcher = function(url, path) {
+  request(url, (error, response, body) => {
+      fs.exists(path, () =>  {
+        rl.question('The file already exist. Do you wish to overwrite it? Y:', (answer) => {
+          if (answer === '\u0059') {
+            fs.writeFile(path, body, () => {
+              const bytes = response.headers['content-length'];
+              console.log(`Downloaded and saved ${bytes} bytes to ${path}`);
+              process.exit()
+            })
+          } else {
+              process.exit()
+          }
+          rl.close();
+        })
+     })
   })
 }
 
-fetcher(url)
+fetcher(url, path)
